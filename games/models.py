ac3ae135f7844a122a7dd401e81b7896462448f2
adriansoghoian <adriansoghoian@gmail.com>
2017-05-07 11:55:17 -0400

    added game model

--- games/models.py
+++ games/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+
+
+class Game(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, default=None)
+	create_date = models.DateTimeField(auto_now_add=True)
+	modified_date = models.DateTimeField(auto_now=True)
+
+
+
