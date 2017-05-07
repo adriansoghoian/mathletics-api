3704e7ccb4b448f0e9cb6d41df3e287795cd95cf
adriansoghoian <adriansoghoian@gmail.com>
2017-05-07 17:02:05 -0400

    tokens showing in the admin finally

--- mathletics/settings.py
+++ mathletics/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'mathletics.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+DATABASES['default'] = dj_database_url.config()
+DATABASES['default']['CONN_MAX_AGE'] = 500
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
