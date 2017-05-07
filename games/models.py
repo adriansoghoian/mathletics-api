fbba5dd4a8e2eef0efff3dabfd4e5f6fecc912ce
adriansoghoian <adriansoghoian@gmail.com>
2017-05-07 12:04:07 -0400

    trying to migrate

--- games/models.py
+++ games/models.py
@@ -13,14 +13,13 @@ class Game(models.Model):
 	game_type = models.CharField(max_length=200, choices=GAME_TYPE)
 
 
+
 class Statistics(models.Model):
 	game = models.ForeignKey(Game)
 	create_date = models.DateTimeField(auto_now_add=True)
 	modified_date = models.DateTimeField(auto_now=True)
-
 	start_time =  models.DateTimeField(default=None)
 	end_time = models.DateTimeField(default=None)
-
 	number_correct = models.IntegerField(default=None)
 	number_incorrect = models.IntegerField(default=None)
 
