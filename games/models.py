5e418b10447419edbf2c7a7a9f426e894e09063f
adriansoghoian <adriansoghoian@gmail.com>
2017-05-07 11:59:20 -0400

    added 2 models

--- games/models.py
+++ games/models.py
@@ -3,11 +3,28 @@ from django.conf import settings
 # Create your models here.
 
 
+GAME_TYPE = [("Multiplication 60","multiplication_60"),]
+
 
 class Game(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, default=None)
 	create_date = models.DateTimeField(auto_now_add=True)
 	modified_date = models.DateTimeField(auto_now=True)
+	game_type = models.CharField(max_length=200, choices=GAME_TYPE)
+
+
+class Statistics(models.Model):
+	game = models.ForeignKey(Game)
+	create_date = models.DateTimeField(auto_now_add=True)
+	modified_date = models.DateTimeField(auto_now=True)
+
+	start_time =  models.DateTimeField(default=None)
+	end_time = models.DateTimeField(default=None)
+
+	number_correct = models.IntegerField(default=None)
+	number_incorrect = models.IntegerField(default=None)
+
+
 
 
 
